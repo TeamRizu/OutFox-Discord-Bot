ebf2b084c637f8463d78daa1583fc989c3b3787f
moruzerinho6 <zerinho6@gmail.com>
2021-09-06 09:53:11 -0300

    add constants for later getrole command

---
+++ src/commands/getrole.js
@@ -0,0 +1,20 @@
+// Libs
+const Discord = require('discord.js')
+
+// Files
+const messageFile = require('../listeners/message.js')
+const languageFile = require('../utils/language.js')
+const buttonFile = require('../utils/buttons.js')
+
+// Variables
+const { MessageActionRow } = Discord
+
+/**
+ *
+ * @param {Discord.Message} message
+ * @param {languageFile.LanguageInstance} language
+ * @param {messageFile.OptionalParams} param3
+ */
+exports.run = async (message, language) => {
+    // TODO: Everything.
+}
