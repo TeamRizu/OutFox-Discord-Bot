5f6208cc831e7e2c7f10c9e351de0072f748620a
zerinho6 <zerinho6@gmail.com>
2022-09-03 18:02:18 -0300

    leaderboard command d.js 14.3.0

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,7 +1,7 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
-const { SlashCommand, ComponentContext } = require('slash-create');
-const { LeaderboardFile } = require('../utils/bhleaderboard.js')
-const LeaderboardSheetInstance = new LeaderboardFile()
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommand, ComponentContext, Message } = require('slash-create');
+const { LeaderboardFile } = require('../utils/bhleaderboard.js');
+const LeaderboardSheetInstance = new LeaderboardFile();
 
 module.exports = class LeaderboardCommand extends SlashCommand {
   constructor(creator) {
@@ -9,7 +9,7 @@ module.exports = class LeaderboardCommand extends SlashCommand {
       name: 'leaderboard',
       description: 'See the leaderboard for OutFox Bug Hunter.'
     });
-    this.commandVersion = '0.0.1'
+    this.commandVersion = '0.0.2';
   }
 
   /**
@@ -18,62 +18,59 @@ module.exports = class LeaderboardCommand extends SlashCommand {
    */
   async run(ctx) {
     await LeaderboardSheetInstance.init();
-    await this.update({
-      interaction: {
-        ctx,
-        values: []
-      },
-      commandArguments: {
-        primalArgument: '0',
-        arguments: ['0'],
-        version: this.commandVersion,
-        firstSend: true
-      }
+    await ctx.defer();
+    const pagesNum = LeaderboardSheetInstance.pages.length
+    const buildEmbed = async (pageIndex) => {
+      const builtPage = await LeaderboardSheetInstance.buildPage(pageIndex);
+      const embed = new EmbedBuilder().setTitle('Project OutFox Bug Hunter Leaderboard').setDescription(builtPage);
+
+      return embed;
+    };
+
+    const buildComponents = (pageIndex) => {
+      const leastPageNum = 0 > pageIndex - 1 ? 0 : pageIndex - 1;
+      const maxPageNum = pageIndex + 1 > pagesNum - 1 ? pagesNum - 1 : pageIndex + 1;
+      const buttons = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId(`back+${leastPageNum}`)
+          .setLabel('Back')
+          .setStyle(ButtonStyle.Primary)
+        ,
+        new ButtonBuilder()
+          .setCustomId(`next+${maxPageNum}`)
+          .setLabel('Next')
+          .setStyle(ButtonStyle.Primary)
+      );
+
+      return [buttons]
+    };
+
+    const embed = await buildEmbed(0)
+    const components = buildComponents(0)
+
+    /**
+     * @type {Message}
+     */
+    const message = await ctx.send({
+      embeds: [embed],
+      components
+    })
+
+    ctx.registerWildcardComponent(message.id, async (cCtx) => {
+      const component = cCtx.customID
+
+      if (!component.startsWith('next') && !component.startsWith('back')) return;
+
+      // Cast to number because buildComponents will try to concat strings otherwise.
+      const page = Number(component.split('+')[1])
+
+      const newEmbed = await buildEmbed(page)
+      const newComponents = buildComponents(page)
+      await cCtx.acknowledge()
+      await message.edit({
+        embeds: [newEmbed],
+        components: newComponents
+      })
     })
   }
-
-  /**
-   *
-   * @param {ComponentContext} ctx
-   * @param {number} pageIndex
-   * @param {boolean} [firstSend]
-   */
-  async update({interaction, commandArguments}) {
-    if (!LeaderboardSheetInstance.pages) {
-      return;
-    }
-    let pageIndex = commandArguments.primalArgument
-    pageIndex = Number(pageIndex)
-    const pagesNum = LeaderboardSheetInstance.pages.length
-    const builtPage = await LeaderboardSheetInstance.buildPage(pageIndex)
-    const embed = new MessageEmbed()
-    .setTitle('Project OutFox Bug Hunter Leaderboard')
-    .setDescription(builtPage)
-
-    const leastPageNum = 0 > (pageIndex - 1) ? 0 : pageIndex - 1
-    const maxPageNum = (pageIndex + 1) > (pagesNum - 1) ? pagesNum - 1 : pageIndex + 1
-
-    const buttons = new MessageActionRow()
-    .addComponents(
-      new MessageButton()
-        .setCustomId(`1━${this.commandVersion}━update━${leastPageNum}`)
-        .setLabel("Back")
-        .setStyle('PRIMARY'),
-      new MessageButton()
-        .setCustomId(`1━${this.commandVersion}━update━${maxPageNum}`)
-        .setLabel('Next')
-        .setStyle('PRIMARY')
-    )
-
-    const msgData = {
-      embeds: [embed],
-      components: [buttons]
-    }
-
-    if (commandArguments.firstSend) {
-      await interaction.ctx.send(msgData)
-    } else {
-      await interaction.ctx.editParent(msgData)
-    }
-  }
-}
+};
