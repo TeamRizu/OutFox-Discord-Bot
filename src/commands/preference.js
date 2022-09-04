9555474c61893095f1659775e39833edec7d6460
zerinho6 <zerinho6@gmail.com>
2022-09-04 18:39:21 -0300

    preference command d.js 14.3.0

--- src/commands/preference.js
+++ src/commands/preference.js
@@ -1,6 +1,6 @@
-const { SlashCommand, ComponentContext } = require('slash-create');
+const { SlashCommand, ComponentContext, Message } = require('slash-create');
 const { LeaderboardMessageFile } = require('../utils/leaderboardMessage.js');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { WikiPreferencesFile } = require('../utils/wikiPreferences.js');
 const PreferencesInstance = new WikiPreferencesFile();
 
@@ -10,7 +10,7 @@ module.exports = class PreferenceCommand extends SlashCommand {
       name: 'preference',
       description: 'Get documentation from any preference.'
     });
-    this.commandVersion = '0.0.1';
+    this.commandVersion = '0.0.2';
   }
 
   /**
@@ -18,112 +18,102 @@ module.exports = class PreferenceCommand extends SlashCommand {
    * @param {ComponentContext} ctx
    */
   async run(ctx) {
-    await this.update({
-      interaction: {
-        ctx,
-        values: []
-      },
-      commandArguments: {
-        primalArgument: 0,
-        arguments: [0, 0],
-        version: this.commandVersion,
-        firstSend: true,
-        commandID: '8'
-      }
-    });
-  }
-
-  async update({ interaction, commandArguments }) {
-    if (interaction.values[0]) {
-      await this.lookUp({
-        interaction,
-        commandArguments: {
-          primalArgument: commandArguments.primalArgument,
-          arguments: interaction.values[0].split('━').slice(3),
-          version: interaction.values[0].split('━')[1],
-          firstSend: false,
-          commandID: interaction.values[0].split('━')[0]
-        }
-      });
-      return;
-    }
-
-    await this.leaderboard({ interaction, commandArguments });
-  }
-
-  async leaderboard({ interaction, commandArguments }) {
-    const page = Number(commandArguments.arguments[1]);
-    const preferencesEmbed = new MessageEmbed()
-      .setTitle('OutFox Wiki - Preferences')
-      .setImage('https://outfox.wiki/logo.png')
-      .setURL('https://outfox.wiki/user-guide/config/preferences/')
-      .setColor('#002c73');
-
-    const LeaderboardMessageInstance = new LeaderboardMessageFile({ interaction, commandArguments });
+    await ctx.defer();
+    const LeaderboardMessageInstance = new LeaderboardMessageFile();
 
     LeaderboardMessageInstance.supportLookUp = true;
     LeaderboardMessageInstance.menuSelectPlaceholder = 'Select Preference to Look Up';
+    LeaderboardMessageInstance.separator = '+';
 
     for (let i = 0; i < PreferencesInstance.preferences.length; i++) {
-      LeaderboardMessageInstance.addElement(PreferencesInstance.preferences[i]);
+      LeaderboardMessageInstance.addElement({
+        description: PreferencesInstance.preferences[i],
+        value: `${i}`
+      });
     }
 
-    preferencesEmbed.setFooter({ text: `Page ${page + 1}/${LeaderboardMessageInstance.pages.pageList.length}` });
-    preferencesEmbed.setDescription(
-      `
-      Preferences.ini is a file that contains many of the system preferences. Some of them are accessible within Project OutFox’s settings menu, but some of them are only accessible by editing the file. Manual edits to Preferences.ini must be performed when the game is closed, or else they may be automatically overwritten.
+    const buildEmbed = (page, LeaderboardInstance) => {
+      const preferencesEmbed = new EmbedBuilder()
+        .setTitle('OutFox Wiki - Preferences')
+        .setImage('https://outfox.wiki/logo.png')
+        .setURL('https://outfox.wiki/user-guide/config/preferences/')
+        .setColor('#002c73')
+        .setFooter({ text: `Page ${page + 1}/${LeaderboardInstance.pages.pageList.length}` })
+        .setDescription(
+          `
+          Preferences.ini is a file that contains many of the system preferences. Some of them are accessible within Project OutFox’s settings menu, but some of them are only accessible by editing the file. Manual edits to Preferences.ini must be performed when the game is closed, or else they may be automatically overwritten.
 
-      It is located in the Save [folder](https://outfox.wiki/user-guide/config/folders).
+          It is located in the Save [folder](https://outfox.wiki/user-guide/config/folders).
 
-      There is over **${PreferencesInstance.preferences.length}** preferences documented.
+          There is over **${PreferencesInstance.preferences.length}** preferences documented.
 
-      ${LeaderboardMessageInstance.pages.pageList[page]}
-      `
-    );
+          ${LeaderboardInstance.pages.pageList[page]}
+          `
+        );
 
-    LeaderboardMessageInstance.page = page;
+      return preferencesEmbed;
+    };
 
-    const msgData = {
-      embeds: [preferencesEmbed],
+    const embed = buildEmbed(0, LeaderboardMessageInstance);
+
+    /**
+     * @type {Message}
+     */
+    const message = await ctx.send({
+      embeds: [embed],
       components: LeaderboardMessageInstance.pageComponents
-    };
+    });
 
-    if (commandArguments.firstSend) {
-      await interaction.ctx.send(msgData);
-    } else {
-      await interaction.ctx.editParent(msgData);
-    }
-  }
+    ctx.registerWildcardComponent(message.id, async (cCtx) => {
+      const component = cCtx.customID;
 
-  async lookUp({ interaction, commandArguments }) {
-    const interactionSplit = interaction.values[0].split('━');
-    const page = Number(interactionSplit[3]);
-    const preferenceName = PreferencesInstance.preferences[page];
-    const documentation = PreferencesInstance.mainObject[PreferencesInstance.preferences[page]];
-    const preferenceEmbed = new MessageEmbed()
-      .setTitle(preferenceName)
-      .setColor('#002c73')
-      .setDescription(documentation);
-    const buttons = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setURL(`https://outfox.wiki/user-guide/config/preferences/#${preferenceName.toLowerCase().replace(' ', '-')}`)
-        .setLabel('See on Page')
-        .setStyle('LINK'),
-      new MessageButton()
-        .setLabel('Another Preference')
-        .setStyle('PRIMARY')
-        .setCustomId(`8━${this.commandVersion}━leaderboard━0━${commandArguments.arguments[1]}`)
-    );
+      if (component === 'startagain') {
+        const newEmbed = buildEmbed(LeaderboardMessageInstance.page, LeaderboardMessageInstance)
+        await cCtx.acknowledge();
+        await message.edit({
+          embeds: [newEmbed],
+          components: LeaderboardMessageInstance.pageComponents
+        });
+      }
 
-    const msgData = {
-      embeds: [preferenceEmbed],
-      components: [buttons]
-    };
+      if (component.startsWith('next') || component.startsWith('back')) {
+        const newPage = Number(component.split('+')[1]);
 
-    if (commandArguments.firstSend) {
-      interaction.ctx.send(msgData);
-    } else {
-      interaction.ctx.editParent(msgData);
-    }
+        LeaderboardMessageInstance.page = newPage;
+
+        const newEmbed = buildEmbed(newPage, LeaderboardMessageInstance);
+
+        await cCtx.acknowledge();
+        await message.edit({
+          embeds: [newEmbed],
+          components: LeaderboardMessageInstance.pageComponents
+        });
+      }
+
+      if (component.startsWith('updatepage')) {
+        const preferenceIndex = Number(cCtx.values[0]);
+        const preferenceName = PreferencesInstance.preferences[preferenceIndex];
+        const documentation = PreferencesInstance.mainObject[preferenceName];
+        const preferenceEmbed = new EmbedBuilder()
+          .setTitle(preferenceName)
+          .setColor('#002c73')
+          .setDescription(documentation);
+        const buttons = new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setURL(
+              `https://outfox.wiki/user-guide/config/preferences/#${preferenceName}`
+            )
+            .setLabel('See on Page')
+            .setStyle(ButtonStyle.Link),
+          new ButtonBuilder().setLabel('Another Preference').setStyle(ButtonStyle.Primary).setCustomId('startagain')
+        );
+
+        await cCtx.acknowledge();
+        await message.edit({
+          embeds: [preferenceEmbed],
+          components: [buttons]
+        });
+      }
+    });
   }
 };
